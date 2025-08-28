b6f139f266658fc7534ecc8b48d8493f664ebef0
Motko <77466238+Motko222@users.noreply.github.com>
2025-08-28 22:33:24 +0200

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,3 +1,4 @@
 read -p "cmd? " c1
-c2=$(echo $c1 | sed 's/--key-name=""/--key-name="my-key"/')
-echo $c2
+c2=./$(echo $c1 | sed 's/--key-name=""/--key-name="my-key"/')
+cd /root/.soundness/cli
+$c2
