05ef41a2cc2560100d191add55b521113c106387
Motko <77466238+Motko222@users.noreply.github.com>
2025-08-29 23:54:24 +0200

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -3,7 +3,9 @@ source $path/env
 
 read -p "cmd? " c1
 c2=./$(echo $c1 | sed 's/--key-name=""/--key-name="my-key"/')
+echo "-------------------------------------------------"
 echo "Password: "$PWD
+echo "-------------------------------------------------"
 cd /root/.soundness/bin
 echo $c2 >cmd
 ./cmd
